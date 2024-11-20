feaf8c6aa5910c34c21cd9824d67c1f3bc92395d
Jordan Bravo <jordan@bravo.cc>
2024-11-20 15:33:46 -0500

    add readme, center 'contacts' section

---
+++ README.md
@@ -0,0 +1,23 @@
+## Setup
+
+- Instal dependencies
+
+```sh
+bundle install
+```
+
+- Start development server
+
+```sh
+bundle exec jekyll serve
+```
+
+Navigate to http://localhost:4000
+
+## Deploy
+
+Push to branch `gh-pages`
+
+## TODO
+
+- Create `/favicon.ico` from avatar.
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My personal website";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ruby
+            bundler
+            jekyll
+            gcc
+            gnumake
+          ];
+        };
+      }
+    );
+}
